65cc262e11c9a29a7cd21f863c46cf487d967b63
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-06 18:11:11 -0700

    Update cte to include is default case

--- dbt_project/models/staging/stg_credit_data.sql
+++ dbt_project/models/staging/stg_credit_data.sql
@@ -2,14 +2,14 @@ with raw as (
   select * from 
   {{ source('raw', 'raw_credit_data') }}
 )
-select 
-  customer_id,
+select
+	customer_id,
     age,
     job,
     credit_amount,
     duration,
     housing,
     purpose,
-    case
-        when risk = 'good' then 0 else 1 end as is_default
-from raw
+	case
+		when risk = 'good' then 0 else 1 end as is_default
+from raw;
