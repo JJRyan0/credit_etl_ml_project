ff55bbe5e72d3d9037cca4a8b5040f86b4ce0856
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-06 18:00:04 -0700

    Create stg_credit_data.sql

---
+++ dbt_project/models/staging/stg_credit_data.sql
@@ -0,0 +1,15 @@
+with raw as (
+  select * from 
+  {{ source('raw', 'raw_credit_data') }}
+)
+select 
+  customer_id,
+    age,
+    job,
+    credit_amount,
+    duration,
+    housing,
+    purpose,
+    case
+        when risk = 'good' then 0 else 1 end as is_default
+from raw
