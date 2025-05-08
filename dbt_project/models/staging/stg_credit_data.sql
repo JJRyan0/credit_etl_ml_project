42a92658a76befc77410762892007e87e6f9e641
John Ryan <johnjoseph.ryan0@gmail.com>
2025-05-07 18:54:06 -0700

    Update stg_credit_data.sql

--- dbt_project/models/staging/stg_credit_data.sql
+++ dbt_project/models/staging/stg_credit_data.sql
@@ -10,6 +10,10 @@ select
     duration,
     housing,
     purpose,
-	case
-		when risk = 'good' then 0 else 1 end as is_default
+	CASE
+    WHEN credit_amount > 15000 AND duration > 36 THEN 1
+    WHEN job IN ('unemployed', 'unskilled') AND housing = 'free' THEN 1
+    WHEN purpose = 'radio/tv' AND credit_amount > 12000 THEN 1
+    ELSE 0
+END AS is_default
 from raw
